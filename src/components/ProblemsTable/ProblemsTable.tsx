d7d92d46c02ce1bb05b447090a6d3d0b7a556667
rohitrk185 <rohitsmudge190@gmail.com>
2024-01-08 22:24:36 +0530

    handle yt player + forgot password

--- src/components/ProblemsTable/ProblemsTable.tsx
+++ src/components/ProblemsTable/ProblemsTable.tsx
@@ -1,58 +1,111 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { problems } from "@/mockProblems/problems";
 import { BsCheckCircle } from "react-icons/bs";
 import { AiFillYoutube } from "react-icons/ai";
+import { IoClose } from "react-icons/io5";
 import Link from "next/link";
+import YouTube from "react-youtube";
 type Props = {};
 
 const ProblemsTable = (props: Props) => {
+  const [youtubePlayer, setYoutubePlayer] = useState({
+    isOpen: false,
+    videoId: ""
+  });
+
+  const handleYtPlayer = (open: boolean, ytVideoId?: string) => {
+    if (typeof ytVideoId === "undefined") return;
+
+    setYoutubePlayer({
+      isOpen: open,
+      videoId: ytVideoId
+    });
+  };
+
+  useEffect(() => {
+    const handleEsc = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        handleYtPlayer(false, "");
+      }
+    };
+
+    window.addEventListener("keydown", handleEsc);
+
+    return () => window.removeEventListener("keydown", handleEsc);
+  }, []);
+
   return (
-    <tbody className="text-white">
-      {problems.map((problem, index) => {
-        const difficultyColor =
-          problem.difficulty.toLowerCase() === "easy"
-            ? "text-dark-green-s"
-            : problem.difficulty.toLowerCase() === "  medium"
-            ? "text-dark-yellow"
-            : "text-dark-pink";
-        return (
-          <tr
-            key={problem.id}
-            className={`${index % 2 === 1 ? "bg-dark-layer-1" : ""}`}
-          >
-            <th className="px-2 py-4 font-medium whitespace-nowrap text-dark-green-s">
-              <BsCheckCircle fontSize={18} width={18} />
-            </th>
+    <>
+      <tbody className="text-white">
+        {problems.map((problem, index) => {
+          const difficultyColor =
+            problem.difficulty.toLowerCase() === "easy"
+              ? "text-dark-green-s"
+              : problem.difficulty.toLowerCase() === "  medium"
+              ? "text-dark-yellow"
+              : "text-dark-pink";
+          return (
+            <tr
+              key={problem.id}
+              className={`${index % 2 === 1 ? "bg-dark-layer-1" : ""}`}
+            >
+              <th className="px-2 py-4 font-medium whitespace-nowrap text-dark-green-s">
+                <BsCheckCircle fontSize={18} width={18} />
+              </th>
 
-            <td className="px-6 py-4">
-              <Link
-                href={`/problems/${problem.id}`}
-                className="hover:text-blue-600 cursor-pointer"
-              >
-                {problem.title}
-              </Link>
-            </td>
+              <td className="px-6 py-4">
+                <Link
+                  href={`/problems/${problem.id}`}
+                  className="hover:text-blue-600 cursor-pointer"
+                >
+                  {problem.title}
+                </Link>
+              </td>
 
-            <td className={`px-6 py-4 ${difficultyColor}`}>
-              {problem.difficulty}
-            </td>
+              <td className={`px-6 py-4 ${difficultyColor}`}>
+                {problem.difficulty}
+              </td>
 
-            <td className="px-6 py-4">{problem.category}</td>
+              <td className="px-6 py-4">{problem.category}</td>
 
-            <td className="px-6 py-4">
-              {problem.videoId ? (
-                <AiFillYoutube
-                  fontSize={25}
-                  className="cursor-pointer hover:text-red-600"
+              <td className="px-6 py-4">
+                {problem.videoId ? (
+                  <AiFillYoutube
+                    fontSize={25}
+                    className="cursor-pointer hover:text-red-600"
+                    onClick={() => handleYtPlayer(true, problem.videoId)}
+                  />
+                ) : (
+                  <p className="text-gray-400">Coming soon...</p>
+                )}
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+
+      {youtubePlayer.isOpen ? (
+        <tfoot className="fixed top-0 left-0 h-screen w-screen flex items-center justify-center ">
+          <div className="bg-black z-10 opacity-70 top-0 left-0 w-screen h-screen absolute"></div>
+          <div className="w-full z-50 h-full px-6 relative max-w-4xl">
+            <div className="w-full h-full flex items-center justify-center relative">
+              <div className="w-full relative">
+                <IoClose
+                  fontSize={"35"}
+                  className={`cursor-pointer absolute -top-10 right-0 hover:text-white`}
+                  onClick={() => handleYtPlayer(false, "")}
                 />
-              ) : (
-                <p className="text-gray-400">Coming soon...</p>
-              )}
-            </td>
-          </tr>
-        );
-      })}
-    </tbody>
+                <YouTube
+                  videoId={youtubePlayer.videoId}
+                  loading="lazy"
+                  iframeClassName="w-full min-h-[500px]"
+                />
+              </div>
+            </div>
+          </div>
+        </tfoot>
+      ) : null}
+    </>
   );
 };
 
--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import Link from "next/link";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/firebase";
+import Logout from "../Buttons/Logout";
+import { useSetRecoilState } from "recoil";
+import { authModalState } from "@/atoms/authModalAtom";
+
 type Props = {};
 
 const Topbar = (props: Props) => {
+  const [user, loading] = useAuthState(auth);
+  const setAuthModalState = useSetRecoilState(authModalState);
+
   return (
     <nav className="relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7">
       <div
@@ -24,11 +33,39 @@ const Topbar = (props: Props) => {
               Premium
             </a>
           </div>
-          <Link href="/auth">
-            <button className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded ">
-              Sign In
-            </button>
-          </Link>
+          {!loading && !user ? (
+            <Link
+              href="/auth"
+              onClick={() => {
+                setAuthModalState((prev) => ({
+                  ...prev,
+                  isOpen: true,
+                  type: "login"
+                }));
+              }}
+            >
+              <button className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded ">
+                Sign In
+              </button>
+            </Link>
+          ) : null}
+          {user ? (
+            <>
+              <div className="cursor-pointer group relative">
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
+                  src="/avatar.png"
+                  alt="User Profile Image"
+                  className="h-8 w-8 rounded-full"
+                />
+
+                <div className="absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg z-40 group-hover:scale-100 scale-0 transition-all duration-300 ease-in-out">
+                  <p className="text-sm">{user.email}</p>
+                </div>
+              </div>
+              <Logout />
+            </>
+          ) : null}
         </div>
       </div>
     </nav>
